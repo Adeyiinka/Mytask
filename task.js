4f56788d27595a7f0b1992598fa7db4a5c9b556c
Adekunle Ibrahim Adeyiinka <helloadeyinka@gmail.com>
2020-03-26 22:00:17 +0100

    My task

---
+++ task.js
@@ -0,0 +1,11 @@
+//My javascript task!
+var Name = ("Adekunle Ibrahim");
+var courses = ["HTML", "CSS", "Javascript"];
+
+console.log("My Name is " + Name);
+console.log("My courses are " + courses);
+
+for (let i = 1; i <= 200; i++){
+    if (i % 2 !==0)
+    console.log(i);
+}
